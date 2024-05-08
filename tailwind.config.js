6de3654692a37a8e789a00778a26c35f4938658a
Jesreel Amorganda <jeamorganda@gbox.ncf.edu.ph>
2024-05-08 19:21:29 +0800

    fix lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx}",
-    "./node_modules/flowbite/**/*.js"
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('flowbite/plugin')
-],
+  ],
 }
-
